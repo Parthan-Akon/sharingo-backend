160bc77fb36c031b685756396c90bae3da4ad7b5
Parthan-Sachin <parthan.parth123@gmail.com>
2020-01-14 21:58:10 +0530

    food record table updated

--- app/src/main/java/com/sharingo/data/foodCallRecord.sql
+++ app/src/main/java/com/sharingo/data/foodCallRecord.sql
@@ -1,8 +1,10 @@
-CREATE TABLE `sharingo`.`foodcallrecord` (
-  `ID` INT NOT NULL AUTO_INCREMENT,
-  `DonorID` VARCHAR(45) NOT NULL,
-  `FoodCount` INT NULL,
-  `FoodItems` VARCHAR(125) NULL,
-  `ServiceCharge` VARCHAR(45) NULL,
-  `OperatorName` VARCHAR(45) NULL,
-  PRIMARY KEY (`ID`));
+CREATE TABLE `foodcallrecord` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `DonorID` int(20) NOT NULL,
+  `FoodCount` int(11) DEFAULT NULL,
+  `FoodItems` varchar(125) DEFAULT NULL,
+  `ServiceCharge` varchar(45) DEFAULT NULL,
+  `OperatorName` varchar(45) DEFAULT NULL,
+  `Inactive` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
