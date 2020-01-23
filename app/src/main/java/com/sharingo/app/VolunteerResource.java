25c3337ef39263dddd0b3b0fc44921859780b41f
Parthan-Sachin <parthan.parth123@gmail.com>
2020-01-24 00:00:33 +0530

    Added post method for volunteer

--- app/src/main/java/com/sharingo/app/VolunteerResource.java
+++ app/src/main/java/com/sharingo/app/VolunteerResource.java
@@ -2,7 +2,10 @@ package com.sharingo.app;
 
 import java.util.List;
 
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -14,8 +17,16 @@ public class VolunteerResource {
 	@Produces({MediaType.APPLICATION_JSON})
 	public List<Volunteer> getAllVolunteers(){
 		
-		VolunteerRepo volunteerRepo = new VolunteerRepo();
-		
+		VolunteerRepo volunteerRepo = new VolunteerRepo();		
 		return volunteerRepo.getVolunteerList();
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	public List<Volunteer> saveVolunteers(JsonObject inputData) {
+		
+		System.out.print(inputData);
+		return null;
 	}
 }
